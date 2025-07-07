6de9b61580e2a3544197c28b16e9d24897945baa
Rafael Rodrigues da Costa <rafaelrdacosta@gmail.com>
2025-07-07 17:13:12 -0300

    Estudo 7 JUL 25

--- 82. aula82_leia um número inteiro maior que 1 e diga se ele é primo.c
+++ 82. aula82_leia um número inteiro maior que 1 e diga se ele é primo.c
@@ -4,28 +4,34 @@
 /*Aula 82
     Faça um programa que peça ao usuário um número inteiro maior que 2 e
     diga se o número informado é primo ou não.
-
 */
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
-    int num, contador;
+    int num, contador = 0, opcao;
 
     do{
-       printf("\nDigite um número inteiro maior que 2: ");
-        scanf("%d", &num);
-    }while(num < 2);
+        do{
+           printf("\nDigite um número inteiro maior que 1: ");
+            scanf("%d", &num);
+        }while(num < 2);
 
-    for(int i = 1; i <= num; i++)
-        if(num % i == 0)
-            contador++;
+        printf("\nDivisores de %d: ", num);
+        for(int i = 2; i <= num/2; i++)
+            if(num % i == 0){
+             printf("%d ", i);
+             contador++;
+            }
 
+        if(contador == 0)
+            printf("\nNúmero %d é primo.\n", num);
+        else
+            printf("\nNúmero %d não é primo.\n", num);
 
-    if(contador == 2)
-        printf("\nNúmero %d é primo.\n", num);
-    else
-        printf("\nNúmero %d não é primo.\n", num);
-
+        printf("\n1 - Digitar outro valor\n");
+        printf("2 - Sair\n");
+        scanf("%d", &opcao);
+    }while(opcao != 2);
 
     return 0;
 }
